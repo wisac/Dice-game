312d4d6d070e6d26c66cead6ddcf5eb34fbaebd1
Isaac Wilson <iswilson.dev@gmail.com>
2023-08-02 17:11:42 +0000

    Refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -5,24 +5,37 @@ const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 score0El.textContent = "0";
 score1El.textContent = "0";
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+//total tracker for current
+let currentScore = 0;
 
 //Hide dice on start
 const diceEl = document.querySelector(".dice");
 diceEl.classList.add("hidden");
 
-const rollBtn = document.querySelector(".btn--roll");
-rollBtn.addEventListener("click", function () {
-    const numberPlayed = randomGenerator(1, 6);
-    diceShower(numberPlayed);
-    console.log(numberPlayed);
-});
-
+//function that generates random numbers within a range
 function randomGenerator(min, max) {
     return Math.floor((Math.random() * (max - min + 1) + min));
-  
+
 }
 
+//function that shows specific dies
 function diceShower(number) {
     diceEl.classList.remove("hidden");
     diceEl.src = `images/dice-${number}.png`;
 }
+
+const rollBtn = document.querySelector(".btn--roll");
+rollBtn.addEventListener("click", function () {
+    //generate random number from 1 to 6
+    const numberPlayed = randomGenerator(1, 6);
+
+    //display corresponding dice to number
+    diceShower(numberPlayed);
+    console.log(numberPlayed);
+
+    
+});
+
