c4f04ee989c5847ed1ebc1f7050283151a74eb0e
Isaac Wilson <iswilson.dev@gmail.com>
2023-08-02 16:42:11 +0000

    feat: show rolled dice to screen

--- src/index.js
+++ src/index.js
@@ -7,5 +7,22 @@ score0El.textContent = "0";
 score1El.textContent = "0";
 
 //Hide dice on start
-const dice = document.querySelector(".dice");
-dice.classList.add("hidden");
+const diceEl = document.querySelector(".dice");
+diceEl.classList.add("hidden");
+
+const rollBtn = document.querySelector(".btn--roll");
+rollBtn.addEventListener("click", function () {
+    const numberPlayed = randomGenerator(1, 6);
+    diceShower(numberPlayed);
+    console.log(numberPlayed);
+});
+
+function randomGenerator(min, max) {
+    return Math.floor((Math.random() * (max - min + 1) + min));
+  
+}
+
+function diceShower(number) {
+    diceEl.classList.remove("hidden");
+    diceEl.src = `images/dice-${number}.png`;
+}
