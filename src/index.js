1adc6f3882fdde58ea903d610f0cbc563f89e65f
Isaac Wilson <iswilson.dev@gmail.com>
2023-08-02 14:52:56 +0000

    update: set game start points

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+//Selecting html elements
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+score0El.textContent = "0";
+score1El.textContent = "0";
+
+//Hide dice on start
+const dice = document.querySelector(".dice");
+dice.classList.add("hidden");
